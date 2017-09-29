bb354b2eae085d706582528b144709197beb6064
Martin O'Reilly <developer@mtspace.net>
2017-09-29 01:10:24 +0100

    Support local warc.gz file as input

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 Scripts for processing common crawl web content through Mozilla readability.js
 
 ## Usage
-`python warc_to_readable_pages.py -i <url-to-warc.gz> -o <output-directory>`
+`python warc_to_readable_pages.py (-f <path-to-warc.gz> | -u <url-to-warc.gz>) -o <output-directory>`
 
 Constraints:
-- URL must be open access (no authentication)
-- URL points to a gzipped WARC file
+- URL or local path refers to a gzipped WARC file
 - The response contents are extracted from each WARC record in the file and
 written to disk as one file per page with response headers stripped
 - Output files are named `<WARC-Record-ID>.txt`. The `.txt` extension allows
--- warc_to_readable_pages.py
+++ warc_to_readable_pages.py
@@ -10,14 +10,19 @@ DEFAULT_MAX_PAGES = 50
 def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description=__name__)
-    parser.add_argument('--input-url', '-i',
-        help='Path to read input WARC file from.')
+    parser.add_argument('--source-url', '-u',
+        help='Remote URL to read input WARC file from.')
+    parser.add_argument('--source-file', '-f',
+        help='Local path to read input WARC file from.')
     parser.add_argument('--output-dir', '-o',
         help='Directory to write processed web pages to.')
     parser.add_argument('--max-pages', '-m',
         help='Maximum number of web pages to process from WARC file.')
 
     args = parser.parse_args()
+    # Validate argumentsif(args.command in ['setup-pool'] and args.pool_directory == None):
+    if not(args.source_file or args.source_url):
+        parser.error("--source-file or --source-url argument must be provided.")
 
     if args.max_pages is None:
         args.max_pages = DEFAULT_MAX_PAGES
@@ -26,12 +31,15 @@ def main():
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    # Open a connection pool
-    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-    # Open a streaming connection to the specified URL
-    r = http.request('GET', args.input_url, preload_content=False)
-    # Wrap the URL filestream in a streamable unzipper
-    f = warc.WARCFile(fileobj=GzipStreamFile(r))
+    if args.source_file is not None:
+        cf = open(args.source_file)
+    elif args.source_url is not None:
+        # Open a connection pool
+        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        # Open a streaming connection to the specified URL
+        cf = http.request('GET', args.source_url, preload_content=False)
+    # Wrap the filestream in a streamable unzipper
+    f = warc.WARCFile(fileobj=GzipStreamFile(cf))
     count = 0
     for record in f:
         if record['WARC-Type'] == 'response':
